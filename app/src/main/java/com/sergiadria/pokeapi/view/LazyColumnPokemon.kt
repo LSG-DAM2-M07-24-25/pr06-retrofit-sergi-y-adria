7bdf24ec38499f0d4fb72c767defd92489fd7548
AuraTurqesa <157357938+AuraTurqesa@users.noreply.github.com>
2025-01-29 10:57:57 +0100

    feat Importar funció items

--- app/src/main/java/com/sergiadria/pokeapi/view/LazyColumnPokemon.kt
+++ app/src/main/java/com/sergiadria/pokeapi/view/LazyColumnPokemon.kt
@@ -3,8 +3,10 @@ package com.sergiadria.pokeapi.view
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
