327fa9505a20fb65123fd3dd8b0c302af375ed3d
AuraTurqesa <157357938+AuraTurqesa@users.noreply.github.com>
2025-02-09 13:15:06 +0100

    feat modificacio APIInterface

--- app/src/main/java/com/sergiadria/pokeapi/api/APIInterface.kt
+++ app/src/main/java/com/sergiadria/pokeapi/api/APIInterface.kt
@@ -1,11 +1,13 @@
 package com.sergiadria.pokeapi.api
 
 import com.sergiadria.pokeapi.model.ApiResponse
+import com.sergiadria.pokeapi.model.Pokemon
 import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface APIInterface {
@@ -13,8 +15,12 @@ interface APIInterface {
     @GET("pokemon")
     suspend fun getPokemons(@Query("limit") limit: Int): Response<ApiResponse>
 
+    @GET("pokemon/{name}")
+    suspend fun getPokemonByName(@Path("name") name: String): Response<Pokemon> // Corregit l'ús del Path
+
     companion object {
-        val BASE_URL = "https://pokeapi.co/api/v2/"
+        private const val BASE_URL = "https://pokeapi.co/api/v2/"
+
         fun create(): APIInterface {
             val client = OkHttpClient.Builder().build()
             val retrofit = Retrofit.Builder()
@@ -25,5 +31,4 @@ interface APIInterface {
             return retrofit.create(APIInterface::class.java)
         }
     }
-
 }
