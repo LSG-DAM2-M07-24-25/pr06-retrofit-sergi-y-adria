941144291af9c0a5cdb70b4ec465bc6bb5be7ebb
AuraTurqesa <157357938+AuraTurqesa@users.noreply.github.com>
2025-02-09 13:15:54 +0100

    feat modificacio Repository

--- app/src/main/java/com/sergiadria/pokeapi/api/Repository.kt
+++ app/src/main/java/com/sergiadria/pokeapi/api/Repository.kt
@@ -1,8 +1,17 @@
 package com.sergiadria.pokeapi.api
 
+import com.sergiadria.pokeapi.model.Pokemon
+import retrofit2.Response
+import com.sergiadria.pokeapi.api.APIInterface
+
 class Repository {
 
     val apiInterface = APIInterface.create()
 
     suspend fun getPokemons(limit: Int) = apiInterface.getPokemons(limit)
+
+    // Funció suspendida per obtenir el Pokémon per nom
+    suspend fun getPokemonByName(name: String): Response<Pokemon> {
+        return apiInterface.getPokemonByName(name)
+    }
 }
