3dc948ef56b52d287e5bb782d267145a1ffa90de
AuraTurqesa <157357938+AuraTurqesa@users.noreply.github.com>
2025-02-09 13:09:08 +0100

    feat Modificació PokemonViewModel per veure detalls

--- app/src/main/java/com/sergiadria/pokeapi/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/com/sergiadria/pokeapi/viewmodel/PokemonViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sergiadria.pokeapi.api.Repository
 import com.sergiadria.pokeapi.model.ApiResponse
+import com.sergiadria.pokeapi.model.Pokemon
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,7 +17,10 @@ class PokemonViewModel : ViewModel() {
     val loading = _loading
     private val _apiResponse = MutableLiveData<ApiResponse>()
     val apiResponse = _apiResponse
+    private val _pokemonDetails = MutableLiveData<Pokemon>()
+    val pokemonDetails = _pokemonDetails
 
+    // Funció per obtenir tots els pokémons
     fun getPokemons() {
         CoroutineScope(Dispatchers.IO).launch {
             val response = repository.getPokemons(10000)
@@ -31,4 +35,17 @@ class PokemonViewModel : ViewModel() {
         }
     }
 
+    // Funció per obtenir un Pokémon per nom
+    fun getPokemonByName(name: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = repository.getPokemonByName(name) // Hauries de tenir aquest mètode a l'API
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    _pokemonDetails.value = response.body()
+                } else {
+                    Log.e("Error :", response.message())
+                }
+            }
+        }
+    }
 }
