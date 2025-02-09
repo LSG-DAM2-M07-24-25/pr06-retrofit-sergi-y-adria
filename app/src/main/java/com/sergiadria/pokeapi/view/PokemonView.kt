980d92134ea9db6bf5f6f5937b62a5a670108a1a
AuraTurqesa <157357938+AuraTurqesa@users.noreply.github.com>
2025-02-09 13:13:43 +0100

    feat modificacio PokemonView.

--- app/src/main/java/com/sergiadria/pokeapi/view/PokemonView.kt
+++ app/src/main/java/com/sergiadria/pokeapi/view/PokemonView.kt
@@ -1,44 +1,49 @@
 package com.sergiadria.pokeapi.view
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.sergiadria.pokeapi.model.ApiResponse
 import com.sergiadria.pokeapi.viewmodel.PokemonViewModel
 
 @Composable
-fun PokemonView(viewModel: PokemonViewModel) {
+fun PokemonView(navController: NavController, viewModel: PokemonViewModel) {
+    // Observant l'estat de la càrrega i la resposta de l'API
     val showLoading: Boolean by viewModel.loading.observeAsState(true)
     val apiResponse: ApiResponse by viewModel.apiResponse.observeAsState(ApiResponse(0, null, null, emptyList()))
-    viewModel.getPokemons()
+
+    // Llamada per obtenir els Pokémon només una vegada quan el composable s'inicia
+    LaunchedEffect(Unit) {
+        // Es fa la crida només si la llista de pokemons està buida
+        if (apiResponse.pokemons.isEmpty()) {
+            viewModel.getPokemons()
+        }
+    }
 
     BoxWithConstraints {
         val isWideScreen = maxWidth > 600.dp
 
         if (showLoading) {
-            Row(
-                modifier = Modifier.fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                CircularProgressIndicator(color = MaterialTheme.colorScheme.secondary)
-            }
+            CircularProgressIndicator(color = MaterialTheme.colorScheme.secondary)
         } else {
             if (isWideScreen) {
-                PokemonGrid(apiResponse.pokemons) // Mostra en format grid si la pantalla és ample
+                // Passar la llista de pokemons i modificar adequadament
+                PokemonGrid(
+                    navController = navController,
+                    pokemons = apiResponse.pokemons, // Passant la llista de pokemons
+                    modifier = Modifier.fillMaxSize() // Assignant un modificador per al disseny
+                )
             } else {
-                PokemonList(apiResponse.pokemons) // Mostra en llista si la pantalla és petita
+                // Passar la llista de pokemons a la funció PokemonList
+                PokemonList(navController = navController, pokemons = apiResponse.pokemons)
             }
         }
     }
