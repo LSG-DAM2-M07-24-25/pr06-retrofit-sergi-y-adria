e8f47890c1ce4dec574e5aed5236d8ed489f3b2a
AuraTurqesa <157357938+AuraTurqesa@users.noreply.github.com>
2025-02-09 13:07:38 +0100

    feat modificació MainActivity.

--- app/src/main/java/com/sergiadria/pokeapi/MainActivity.kt
+++ app/src/main/java/com/sergiadria/pokeapi/MainActivity.kt
@@ -1,26 +1,23 @@
 package com.sergiadria.pokeapi
 
+import com.sergiadria.pokeapi.view.PokemonView
+import android.annotation.SuppressLint
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.sergiadria.pokeapi.model.ApiResponse
-import com.sergiadria.pokeapi.model.Pokemon
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
 import com.sergiadria.pokeapi.ui.theme.PokeApiTheme
-import com.sergiadria.pokeapi.view.PokemonView
+import com.sergiadria.pokeapi.view.PokemonDetailScreen
 import com.sergiadria.pokeapi.viewmodel.PokemonViewModel
 
 class MainActivity : ComponentActivity() {
@@ -32,19 +29,40 @@ class MainActivity : ComponentActivity() {
         setContent {
             PokeApiTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    PokemonView(viewModel = pokemonViewModel)
+                    val navController = rememberNavController()
+
+                    NavHost(navController, startDestination = "pokemon_list") {
+                        composable("pokemon_list") {
+                            PokemonView(navController, pokemonViewModel)
+                        }
+                        composable(
+                            "pokemon_detail/{pokemonName}",
+                            arguments = listOf(navArgument("pokemonName") { type = NavType.StringType })
+                        ) { backStackEntry ->
+                            val pokemonName = backStackEntry.arguments?.getString("pokemonName") ?: "Sense nom"
+                            val pokemon = pokemonViewModel.getPokemonByName(pokemonName) // Necessites obtenir el Pokémon per nom
+                            PokemonDetailScreen(pokemon = pokemon)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+
+@SuppressLint("ViewModelConstructorInComposable")
 @Preview(showBackground = true)
 @Composable
 fun PokeApiPreview() {
     PokeApiTheme {
-        PokemonView(viewModel = PokemonViewModel())
+        val navController = rememberNavController()
+        PokemonView(
+            viewModel = PokemonViewModel(),
+            navController = navController
+        )
     }
 }
